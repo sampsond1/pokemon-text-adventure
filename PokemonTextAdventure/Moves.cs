fd466b2481a018b785263b4f86e99206c4fbb8fa
sampsond1 <82178664+sampsond1@users.noreply.github.com>
2021-04-14 15:44:02 -0600

    Update Moves.cs
    Added more properties to support status conditions and special moves.

--- PokemonTextAdventure/Moves.cs
+++ PokemonTextAdventure/Moves.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,31 @@ namespace PokemonTextAdventure
 {
     public class Move
     {
-        public string moveName;
-        public int moveDamage;
-        public int moveAccuracy;
+        public string name;
+        public int damage;
+        public int accuracy;
+        public int numberTimesHit;
+        public int repeatingDamage;
+        public int turnsInactive;
+        public int attackBoost;
 
         public Move(string _moveName, int _moveDamage, int _moveAccuracy)
+        {
+            name = _moveName;
+            damage = _moveDamage;
+            accuracy = _moveAccuracy;
+
+        }
+        
+        public Move(string _moveName, int _moveDamage, int _moveAccuracy, int _numberTimesHit, int _repeatingDamage, int turnsInactive, int attackBoost)
         {
             moveName = _moveName;
             moveDamage = _moveDamage;
             moveAccuracy = _moveAccuracy;
-
+            numberTimesHit = _numberTimesHit;
+            repeatingDamage = _repeatingDamage;
+            turnsInactive = _turnsInactive;
+            attackBoost = _attackBoost;
         }
     }
 }
